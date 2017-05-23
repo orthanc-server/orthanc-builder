a298187e6a5e9b101c0169b53cff37a90048e985
Alain Mazy <am@osimis.io>
2017-05-23 11:10:25 +0200

    including WebViewer pro in orthanc-pro -> 17.5.beta package

--- ciBuildOsimisOrthancProDockerImage.sh
+++ ciBuildOsimisOrthancProDockerImage.sh
@@ -10,6 +10,11 @@ cd $root/docker
 # let's build the 'pro image'
 mkdir -p binaries/plugins-pro
 
+# fetch mssql so file
 wget orthanc.osimis.io/docker-so/mssql/0.4.0/libOrthancMsSqlIndex.so -O binaries/plugins-pro/libOrthancMsSqlIndex.so # CHANGE_VERSION
 
-docker build $@ -t osimis/orthanc-pro:17.5.alpha -f orthanc-pro/Dockerfile . # CHANGE_VERSION
+
+containerId=$(docker create osimis/osimis-webviewer-pro:09431e1) # CHANGE_VERSION
+docker cp $containerId:/usr/share/orthanc/plugins/libOsimisWebViewerPro.so binaries/plugins-pro/
+
+docker build $@ -t osimis/orthanc-pro:17.5.beta -f orthanc-pro/Dockerfile . # CHANGE_VERSION
