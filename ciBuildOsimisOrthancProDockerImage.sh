438a869db3da81e0e9627bc52c1ffc9e358b066b
Thibault Nélis <tn@osimis.io>
2017-07-03 14:31:15 +0200

    Normalize procedures
    Go through env in shebang to lookup interpreters in PATH.
    Add last-line EOL special character.
    Properly quote arguments.
    Use long-form options.
    Remove unnecessary spacing.
    Simplify some operations (e.g. grab root and change dir).

--- ciBuildOsimisOrthancProDockerImage.sh
+++ ciBuildOsimisOrthancProDockerImage.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+set -o xtrace
 
-set -x #to debug the script
-set -e #to exit the script at the first failure
-
-root=${REPOSITORY_PATH:-$(git rev-parse --show-toplevel)}
-
-cd $root/docker
+cd "${REPOSITORY_PATH:-$(git rev-parse --show-toplevel)}/docker"
 
 # let's build the 'pro image'
-mkdir -p binaries/plugins-pro
+mkdir --parents binaries/plugins-pro
 
 # fetch mssql so file
 wget orthanc.osimis.io/docker-so/mssql/0.4.1/libOrthancMsSqlIndex.so -O binaries/plugins-pro/libOrthancMsSqlIndex.so # CHANGE_VERSION
@@ -21,11 +18,10 @@ function onExit {
 }
 trap onExit EXIT
 
-
 viewerContainerId=$(docker create osimis/osimis-webviewer-pro:f017049) # CHANGE_VERSION
-function removeOsimisWebViewer { docker rm $viewerContainerId; }
+function removeOsimisWebViewer { docker rm "$viewerContainerId"; }
 exitHandlers+=(removeOsimisWebViewer)
 
-docker cp $viewerContainerId:/usr/share/orthanc/plugins/libOsimisWebViewerPro.so binaries/plugins-pro/
+docker cp "$viewerContainerId:/usr/share/orthanc/plugins/libOsimisWebViewerPro.so" binaries/plugins-pro/
 
-docker build $@ -t osimis/orthanc-pro:17.6.1 -f orthanc-pro/Dockerfile . # CHANGE_VERSION
+docker build "$@" --tag=osimis/orthanc-pro:17.6.1 --file=orthanc-pro/Dockerfile . # CHANGE_VERSION
