93dcb99822c74682dab006d07a7c8f36073950b1
Thibault Nélis <tn@osimis.io>
2017-07-13 17:03:49 +0200

    Checkout target versions to build for dependencies

--- docker/builder/orthanc-plugins/build-azure-storage-cpp.sh
+++ docker/builder/orthanc-plugins/build-azure-storage-cpp.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-git clone https://github.com/Azure/azure-storage-cpp.git
+git clone "--branch=v$1" \
+	--single-branch \
+	https://github.com/Azure/azure-storage-cpp.git
 cd azure-storage-cpp/Microsoft.WindowsAzure.Storage/
 mkdir build.release
 cd build.release
--- docker/builder/orthanc-plugins/build-casablanca.sh
+++ docker/builder/orthanc-plugins/build-casablanca.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
-git clone https://github.com/Microsoft/cpprestsdk.git casablanca
+git clone "--branch=v$1" \
+	--single-branch \
+	https://github.com/Microsoft/cpprestsdk.git \
+	casablanca
 cd casablanca/Release
 mkdir build.release
 cd build.release
