ac5c09de76ffbb99b066c9a780e342b4b3a91e87
Thibault Nélis <tn@osimis.io>
2017-07-28 15:32:25 +0200

    Allow use of environment variables to define secrets
    Not recommended, but required where Swarm mode isn't available.

--- docker/orthanc-pro/setup.d/wvp.sh
+++ docker/orthanc-pro/setup.d/wvp.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -o errexit
-licenseStringFile=/run/secrets/${WVP_LICENSE_STRING_SECRET:-wvp-licensestring}
 
-if [[ ! -e "$licenseStringFile" ]]; then
-	echo "WVP-SETUP: Use wvp-licensestring Docker secret to enable" >&2
-	echo "WVP-SETUP: Fallback to Osimis Web Viewer Basic" >&2
-	exit 0
+if [[ $WVP_LICENSE_STRING ]]; then
+	licenseString=$WVP_LICENSE_STRING
+else
+	licenseStringFile=/run/secrets/${WVP_LICENSE_STRING_SECRET:-wvp-licensestring}
+
+	if [[ ! -e "$licenseStringFile" ]]; then
+		echo "WVP-SETUP: Use wvp-licensestring Docker secret to enable" >&2
+		echo "WVP-SETUP: Fallback to Osimis Web Viewer Basic" >&2
+		exit 0
+	fi
+
+	licenseString=$(<"$licenseStringFile")
 fi
 
-echo "WVP-SETUP: Found license, writing wvp.json, disabling WVB, enabling WVP" >&2
-licenseString=$(<"$licenseStringFile")
+echo "WVP-SETUP: Found license, overwriting osimis-webviewer.json, disabling WVB, enabling WVP" >&2
 cat <<EOF >/etc/orthanc/osimis-webviewer.json
 {
 	"WebViewer": {
