db1a72f215b0e667d5fa65ede4e43b1e8118c405
Thibault Nélis <tn@osimis.io>
2017-09-18 16:29:47 +0200

    azstor, mssql, wvp: Use new setup procedure format

--- docker/orthanc-pro/setup.d/wvp.sh
+++ docker/orthanc-pro/setup.d/wvp.sh
@@ -1,31 +1,18 @@
-#!/usr/bin/env bash
-set -o errexit
-
-if [[ $WVP_LICENSE_STRING ]]; then
-	licenseString=$WVP_LICENSE_STRING
-else
-	licenseStringFile=/run/secrets/${WVP_LICENSE_STRING_SECRET:-wvp-licensestring}
-
-	if [[ ! -e "$licenseStringFile" ]]; then
-		echo "WVP-SETUP: Use wvp-licensestring Docker secret to enable" >&2
-		echo "WVP-SETUP: Fallback to Osimis Web Viewer Basic" >&2
-		exit 0
-	fi
-
-	licenseString=$(<"$licenseStringFile")
-fi
-
-echo "WVP-SETUP: Found license, overwriting osimis-webviewer.json, disabling WVB, enabling WVP" >&2
-cat <<EOF >/etc/orthanc/osimis-webviewer.json
-{
-	"WebViewer": {
-		"StudyDownloadEnabled": ${WVP_STUDY_DOWNLOAD:-true},
-		"VideoDisplayEnabled": ${WVP_VIDEO:-true},
-		"AnnotationStorageEnabled": ${WVP_ANNOTATIONS_STORAGE:-false},
-		"LiveshareEnabled": ${WVP_LIVESHARE:-false},
-		"LicenseString": "$licenseString"
+name=WVP
+conf=osimis-webviewer
+settings=(STUDY_DOWNLOAD VIDEO ANNOTATIONS_STORAGE LIVESHARE LICENSE_STRING)
+secrets=(LICENSE_STRING)
+plugin=libOsimisWebViewerPro
+function genconf {
+	cat <<-EOF >/etc/orthanc/osimis-webviewer.json
+	{
+		"WebViewer": {
+			"StudyDownloadEnabled": ${STUDY_DOWNLOAD:-true},
+			"VideoDisplayEnabled": ${VIDEO:-true},
+			"AnnotationStorageEnabled": ${ANNOTATIONS_STORAGE:-false},
+			"LiveshareEnabled": ${LIVESHARE:-false},
+			"LicenseString": "$LICENSE_STRING"
+		}
 	}
+	EOF
 }
-EOF
-mv /usr/share/orthanc/plugins{,-disabled}/libOsimisWebViewer.so
-mv /usr/share/orthanc/plugins{-disabled,}/libOsimisWebViewerPro.so
