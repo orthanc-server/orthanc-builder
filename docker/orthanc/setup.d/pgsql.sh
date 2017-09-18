490ef6e78daa6d78ca5c3ef45e9f2ac15eff0de5
Thibault Nélis <tn@osimis.io>
2017-09-18 16:08:31 +0200

    Strip setup proc prefixes (http-port, pgsql)

--- docker/orthanc/setup.d/pgsql.sh
+++ docker/orthanc/setup.d/pgsql.sh
@@ -7,14 +7,14 @@ function genconf {
 	cat <<-EOF >$1
 	{
 		"PostgreSQL": {
-			"EnableIndex": ${PG_INDEX:-false},
-			"EnableStorage": ${PG_STORAGE:-false},
-			"Host": "$PG_HOST",
-			"Port": ${PG_PORT:-5432},
-			"Database": "${PG_DB:-postgres}",
-			"Username": "${PG_USER:-postgres}",
-			"Password": "$PG_PASSWORD",
-			"Lock": ${PG_LOCK:-false}
+			"EnableIndex": ${INDEX:-false},
+			"EnableStorage": ${STORAGE:-false},
+			"Host": "$HOST",
+			"Port": ${PORT:-5432},
+			"Database": "${DB:-postgres}",
+			"Username": "${USER:-postgres}",
+			"Password": "$PASSWORD",
+			"Lock": ${LOCK:-false}
 		}
 	}
 	EOF
