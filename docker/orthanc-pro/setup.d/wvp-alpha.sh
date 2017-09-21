4b06b3d159dd27295eabca26258a45c857f66484
Alain Mazy <am@osimis.io>
2017-09-21 06:59:33 +0000

    wvp-alpha.sh edited online with Bitbucket

--- docker/orthanc-pro/setup.d/wvp-alpha.sh
+++ docker/orthanc-pro/setup.d/wvp-alpha.sh
@@ -1,17 +1,17 @@
 # WARNING: conflicts with WVP (stable)
 name=WVP_ALPHA
 conf=osimis-webviewer-alpha
-settings=(STUDY_DOWNLOAD VIDEO ANNOTATIONS_STORAGE LIVESHARE LICENSE_STRING)
+settings=(STUDY_DOWNLOAD_ENABLED VIDEO_ENABLED ANNOTATIONS_STORAGE_ENABLED LIVESHARE_ENABLED LICENSE_STRING)
 secrets=(LICENSE_STRING)
 plugin=libOsimisWebViewerProAlpha
 function genconf {
 	cat <<-EOF >"$1"
 	{
 		"WebViewer": {
-			"StudyDownloadEnabled": ${STUDY_DOWNLOAD:-true},
-			"VideoDisplayEnabled": ${VIDEO:-true},
-			"AnnotationStorageEnabled": ${ANNOTATIONS_STORAGE:-false},
-			"LiveshareEnabled": ${LIVESHARE:-false},
+			"StudyDownloadEnabled": ${STUDY_DOWNLOAD_ENABLED:-true},
+			"VideoDisplayEnabled": ${VIDEO_ENABLED:-true},
+			"AnnotationStorageEnabled": ${ANNOTATIONS_STORAGE_ENABLED:-false},
+			"LiveshareEnabled": ${LIVESHARE_ENABLED:-false},
 			"LicenseString": "$LICENSE_STRING"
 		}
 	}
