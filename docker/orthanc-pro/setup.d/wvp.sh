1b01c4250aa4ab0c35b035ac0be8fd660337c6bb
Alain Mazy <am@osimis.io>
2017-09-27 19:15:52 +0200

    cleanup + doc

--- docker/orthanc-pro/setup.d/wvp.sh
+++ docker/orthanc-pro/setup.d/wvp.sh
@@ -1,16 +1,16 @@
 name=WVP
 conf=osimis-webviewer
-settings=(STUDY_DOWNLOAD VIDEO ANNOTATIONS_STORAGE LIVESHARE LICENSE_STRING)
+settings=(STUDY_DOWNLOAD_ENABLED VIDEO ANNOTATIONS_STORAGE LIVESHARE LICENSE_STRING)
 secrets=(LICENSE_STRING)
 plugin=libOsimisWebViewerPro
 function genconf {
 	cat <<-EOF >"$1"
 	{
 		"WebViewer": {
-			"StudyDownloadEnabled": ${STUDY_DOWNLOAD:-true},
-			"VideoDisplayEnabled": ${VIDEO:-true},
-			"AnnotationStorageEnabled": ${ANNOTATIONS_STORAGE:-false},
-			"LiveshareEnabled": ${LIVESHARE:-false},
+			"StudyDownloadEnabled": ${STUDY_DOWNLOAD_ENABLED:-true},
+			"VideoDisplayEnabled": ${VIDEO_ENABLED:-true},
+			"AnnotationStorageEnabled": ${ANNOTATIONS_STORAGE_ENABLED:-false},
+			"LiveshareEnabled": ${LIVESHARE_ENABLED:-false},
 			"LicenseString": "$LICENSE_STRING"
 		}
 	}
---
+++ docker/orthanc/setup.d/general.sh
@@ -0,0 +1,12 @@
+name=GENERAL
+conf=general
+settings=(NAME)
+default=true
+function genconf {
+	# TODO use more descriptive settings names
+	cat <<-EOF >"$1"
+	{
+		"Name": "${NAME:-ORTHANC}"
+	}
+	EOF
+}
