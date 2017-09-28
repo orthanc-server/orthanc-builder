c7bcd18b62f1efe0c2b404178ebf91e77eff653f
Alain Mazy <am@osimis.io>
2017-09-28 11:24:42 +0200

    finalizing

--- docker/orthanc/docker-entrypoint.sh
+++ docker/orthanc/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
-set -e
-set -x
 base=/usr/lib/orthanc
 
 # call setup.sh on each setup procedure script found in $base/setup.d (one for each plugin).
 find "$base/setup.d" -type f -exec "$base/setup.sh" "{}" ";"
+
+echo "Startup command: Orthanc $@"
 exec Orthanc "$@"
