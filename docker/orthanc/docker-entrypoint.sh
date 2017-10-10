a2fd2763275d744a9219bf1de7e34c33fe3a0f0f
Thibault Nélis <tn@osimis.io>
2017-10-10 12:05:20 +0200

    Output logs to stderr

--- docker/orthanc/docker-entrypoint.sh
+++ docker/orthanc/docker-entrypoint.sh
@@ -5,5 +5,5 @@ base=/usr/lib/orthanc
 # call setup.sh on each setup procedure script found in $base/setup.d (one for each plugin).
 find "$base/setup.d" -type f -exec "$base/setup.sh" "{}" ";"
 
-echo "Startup command: Orthanc $@"
+echo "Startup command: Orthanc $@" >&2
 exec Orthanc "$@"
