9e16fc8fbe6fd85e1da69b820ac15d672ed078f6
Alain Mazy <am@osimis.io>
2017-10-11 18:25:18 +0200

    added args to build.sh to build all images and eventually push them to dockerhub

--- build.sh
+++ build.sh
@@ -4,3 +4,32 @@ set -o xtrace
 ./ciBuildOrthancBuilderImage.sh
 ./ciBuildOsimisOrthancDockerImage.sh
 ./ciBuildOsimisOrthancProDockerImage.sh
+
+
+version=$1
+tagLatest=${2:-false}
+push=${3:-false}
+
+if [[ ! $version ]]; then
+	echo "please provide a tag in argument" >&2
+	exit 1
+fi
+
+docker tag osimis/orthanc:current osimis/orthanc:"$version"
+docker tag osimis/orthanc-pro:current osimis/orthanc-pro:"$version"
+
+if [[ $push == true ]]; then
+	docker push osimis/orthanc:"$version"
+	docker push osimis/orthanc-pro:"$version"
+fi
+
+if [[ $tagLatest == true ]]; then
+	docker tag osimis/orthanc:current osimis/orthanc:latest
+	docker tag osimis/orthanc-pro:current osimis/orthanc-pro:latest
+
+	if [[ $push == true ]]; then
+		docker push osimis/orthanc:latest
+		docker push osimis/orthanc-pro:latest
+	fi
+
+fi
