a286ea686233f144ee7dc546e6c26eaf23015974
Thibault Nélis <tn@osimis.io>
2017-10-12 14:11:16 +0200

    pgsql: Only enable plugins when actually used

--- docker/orthanc/setup.d/pgsql.sh
+++ docker/orthanc/setup.d/pgsql.sh
@@ -11,11 +11,23 @@ function genconf {
 	if [[ $PASSWORD ]]; then
 		passwordprop="\"Password\": \"$PASSWORD\","
 	fi
+
+	# We need the actual value we eventually pick for the plugin
+	# selection below.  The `:=` syntax doesn't seem to have an
+	# effect when performed within a heredoc (subprocess?) so we
+	# perform the default value logic outside.
+	if [[ ! $INDEX_ENABLED ]]; then
+		INDEX_ENABLED=true
+	fi
+	if [[ ! $STORAGE_ENABLED ]]; then
+		STORAGE_ENABLED=false
+	fi
+
 	cat <<-EOF >"$1"
 	{
 		"PostgreSQL": {
-			"EnableIndex": ${INDEX_ENABLED:-true},
-			"EnableStorage": ${STORAGE_ENABLED:-false},
+			"EnableIndex": $INDEX_ENABLED,
+			"EnableStorage": $STORAGE_ENABLED,
 			"Host": "$HOST",
 			"Port": ${PORT:-5432},
 			"Database": "${DB:-postgres}",
@@ -25,4 +37,17 @@ function genconf {
 		}
 	}
 	EOF
+
+	# PG_ENABLED alone will enable both plugins without generating a
+	# configuration file, then let the user configure which to
+	# enable using their own configuration files.  However, when we
+	# *are* generating the configuration file, we *know* which will
+	# be used and so we only enable them when necessary.
+	plugins=()
+	if [[ $INDEX_ENABLED == true ]]; then
+		plugins+=(libOrthancPostgreSQLIndex)
+	fi
+	if [[ $STORAGE_ENABLED == true ]]; then
+		plugins+=(libOrthancPostgreSQLStorage)
+	fi
 }
