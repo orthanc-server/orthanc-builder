d307a912c5dff0219c1f1bba2f1b49313b47a9d0
Thibault Nélis <tn@osimis.io>
2017-10-31 17:28:28 +0100

    storage: Rename setting, switch to global

--- docker/orthanc/setup.d/storage.sh
+++ docker/orthanc/setup.d/storage.sh
@@ -1,16 +1,15 @@
 name=STORAGE
 conf=storage
-settings=(DIR MAX_SIZE MAX_PATIENTS INDEX_ONLY)
+settings=(DIR MAX_SIZE MAX_PATIENTS)
+globals=(STORE_DICOM)
 default=true
 function genconf {
-	local store=true
-	[[ $INDEX_ONLY == true ]] && store=false
 	cat <<-EOF >"$1"
 	{
 		"StorageDirectory": "${DIR:-/var/lib/orthanc/db}",
 		"MaximumStorageSize": ${MAX_SIZE:-0},
 		"MaximumPatientCount": ${MAX_PATIENTS:-0},
-		"StoreDicom": $store
+		"StoreDicom": ${STORE_DICOM:-true}
 	}
 	EOF
 }
