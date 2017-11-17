e4c049c66dc2b2dcd5db3c5043df662adea19a1e
Thibault Nélis <tn@osimis.io>
2017-11-17 15:07:49 +0100

    http: Add HTTP_CL_PROXY

--- docker/orthanc/setup.d/http.sh
+++ docker/orthanc/setup.d/http.sh
@@ -1,11 +1,12 @@
 name=HTTP
 conf=http
-settings=(CL_TIMEOUT CL_VERIFY_PEERS PORT)
+settings=(CL_TIMEOUT CL_VERIFY_PEERS CL_PROXY PORT)
 function genconf {
 	cat <<-EOF >"$1"
 	{
 		"HttpTimeout": ${CL_TIMEOUT:-0},
 		"HttpsVerifyPeers": ${CL_VERIFY_PEERS:-true},
+		"HttpProxy": "$CL_PROXY",
 		"HttpPort": ${PORT:-8042}
 	}
 	EOF
