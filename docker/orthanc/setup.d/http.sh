1e42760b5587af2f13f4aa80f49db16eeff9ab18
Alain Mazy <am@osimis.io>
2017-12-20 16:42:37 +0100

    include ca-certificates in image and use them by default

--- docker/orthanc/setup.d/http.sh
+++ docker/orthanc/setup.d/http.sh
@@ -1,11 +1,13 @@
 name=HTTP
 conf=http
-settings=(CL_TIMEOUT CL_VERIFY_PEERS CL_PROXY PORT)
+settings=(CL_TIMEOUT CL_VERIFY_PEERS CL_CA_CERTS CL_PROXY PORT)
+default=true
 function genconf {
 	cat <<-EOF >"$1"
 	{
 		"HttpTimeout": ${CL_TIMEOUT:-0},
 		"HttpsVerifyPeers": ${CL_VERIFY_PEERS:-true},
+		"HttpsCACertificates" : "${CL_CA_CERTS:-/etc/ssl/certs/ca-certificates.crt},"
 		"HttpProxy": "$CL_PROXY",
 		"HttpPort": ${PORT:-8042}
 	}
