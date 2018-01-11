a26c44c447604a806bd50527d1f342199eccb83d
Alain Mazy <am@osimis.io>
2018-01-11 12:44:13 +0100

    upgraded WVP_ALPHA to fix the windowingPresets in Lify

--- ciBuildOsimisOrthancProDockerImage.sh
+++ ciBuildOsimisOrthancProDockerImage.sh
@@ -40,7 +40,7 @@ exitHandlers+=(removeOsimisWebViewer)
 
 docker cp --follow-link "$viewerContainerId:/usr/share/orthanc/plugins/libOsimisWebViewerPro.so" binaries/plugins-pro/
 
-viewerContainerIdAlpha=$(docker create osimis/osimis-webviewer-pro:217d8dc) # CHANGE_VERSION
+viewerContainerIdAlpha=$(docker create osimis/osimis-webviewer-pro:13ce4ba) # CHANGE_VERSION
 function removeOsimisWebViewerAlpha { docker rm "$viewerContainerIdAlpha"; }
 exitHandlers+=(removeOsimisWebViewerAlpha)
 
