0d27006a3759e667bbe3287406f59a1ad3e0e7b1
Alain Mazy <am@osimis.io>
2018-01-22 14:27:17 +0100

    added DW_STOW_MAX_INSTANCES & DW_STOW_MAX_SIZE

--- docker/orthanc/setup.d/dicomweb.sh
+++ docker/orthanc/setup.d/dicomweb.sh
@@ -1,6 +1,6 @@
 name=DW
 conf=dicomweb
-settings=(ROOT WADO_URI_ENABLED WADO_URI_ROOT HOST TLS SERVERS)
+settings=(ROOT WADO_URI_ENABLED WADO_URI_ROOT HOST TLS SERVERS STOW_MAX_INSTANCES STOW_MAX_SIZE)
 plugin=libOrthancDicomWeb
 function genconf {
 	# TODO use more descriptive settings names
@@ -13,7 +13,9 @@ function genconf {
 			"WadoRoot": "${WADO_URI_ROOT:-/wado}",
 			"Host": "${HOST:-localhost}",
 			"Ssl": ${TLS:-false},
-			"Servers": ${SERVERS:-"{}"}
+			"Servers": ${SERVERS:-"{}"},
+			"StowMaxInstances": ${STOW_MAX_INSTANCES:-10},
+			"StowMaxSize": ${STOW_MAX_SIZE:-10}
 		}
 	}
 	EOF
