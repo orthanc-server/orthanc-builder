45c69b97334bcba9e1402618929147ccc4c5790b
Alain Mazy <am@osimis.io>
2018-02-19 19:12:33 +0100

    updated orthanc to lua_dll branch

--- ciBuildOrthancBuilderImage.sh
+++ ciBuildOrthancBuilderImage.sh
@@ -21,7 +21,7 @@ docker build --tag=osimis/orthanc-builder-base:current base "$@"
 
 # build the orthanc-builder image (no plugin)
 docker build --tag=osimis/orthanc-builder:current \
-	--build-arg=ORTHANC_VERSION=d9e3781 \
+	--build-arg=ORTHANC_VERSION=6c2c59c \
 	orthanc "$@"
 # CHANGE_VERSION (official version is someting like Orthanc-1.3.0)
 
