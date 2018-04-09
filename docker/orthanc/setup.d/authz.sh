0c3e5a7fd2653aeb75cde0aea41a5cd36b9d072c
amazy <am@osimis.io>
2018-04-09 21:51:41 +0200

    fix

--- docker/orthanc/setup.d/authz.sh
+++ docker/orthanc/setup.d/authz.sh
@@ -2,25 +2,25 @@ name=AUTHZ
 conf=authorization
 plugin=libOrthancAuthorization
 settings=(
-    WEBSERVICE
-    TOKEN_HTTP_HEADERS
-    TOKEN_GET_ARGUMENTS
-    UNCHECKED_RESOURCES
-    UNCHECKED_FOLDERS
-    UNCHECKED_LEVELS
-    )
+	WEBSERVICE
+	TOKEN_HTTP_HEADERS
+	TOKEN_GET_ARGUMENTS
+	UNCHECKED_RESOURCES
+	UNCHECKED_FOLDERS
+	UNCHECKED_LEVELS
+	)
 plugin=libOrthancAuthorization
 function genconf {
-    cat <<-EOF >"$1"
-    {
-        "Authorization": {
-            "WebService": "$WEBSERVICE",
-            "TokenHttpHeaders": ${TOKEN_HTTP_HEADERS:-"[]"},
-            "TokenGetArguments": ${TOKEN_GET_ARGUMENTS:-"[]"},
-            "UncheckedResources": ${UNCHECKED_RESOURCES:-"[]"},
-            "UncheckedFolders": ${UNCHECKED_FOLDERS:-"[]"},
-            "UncheckedLevels": ${UNCHECKED_LEVELS:-"[]"}
-        }
-    }
-    EOF
+	cat <<-EOF >"$1"
+	{
+		"Authorization": {
+			"WebService": "$WEBSERVICE",
+			"TokenHttpHeaders": [$TOKEN_HTTP_HEADERS],
+			"TokenGetArguments": [$TOKEN_GET_ARGUMENTS],
+			"UncheckedResources": [$UNCHECKED_RESOURCES],
+			"UncheckedFolders": [$UNCHECKED_FOLDERS],
+			"UncheckedLevels": [$UNCHECKED_LEVELS]
+		}
+	}
+	EOF
 }
