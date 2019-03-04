6381a958f81269504efc471403a7d210354919a6
Sebastien Jodogne <s.jodogne@gmail.com>
2019-03-04 22:11:26 +0100

    adding transfers accelerator

---
+++ docker/orthanc/setup.d/transfers.sh
@@ -0,0 +1,16 @@
+name=TRANSFERS
+conf=transfers
+settings=(THREADS RETRIES)
+plugin=libOrthancTransfers
+function genconf {
+    # TODO - Consider adding more options:
+    # http://book.orthanc-server.com/plugins/transfers.html#advanced-options
+	cat <<-EOF >"$1"
+	{
+		"Transfers": {
+			"Threads" : ${THREADS:-6},
+			"MaxHttpRetries" : ${RETRIES:0}
+		}
+	}
+	EOF
+}
