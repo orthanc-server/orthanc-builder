23baee91d5e8a57ea0be9d9f03f6aed2d26344b5
Thibault Nélis <tn@osimis.io>
2019-08-20 17:34:43 +0200

    Add gcp-dicom setup procedure
    While we do not have facilities to handle dynamic environment variables
    (e.g. to create complex lists), it is still useful to reify as many
    settings as possible as environment variables so that higher-level
    tooling can handle them in isolation (among other things).  So, we
    decide to establish a "single account" mode which should cover the vast
    majority of cases, and still offer an escape hatch in the form of the
    ACCOUNTS setting that allows specifying everything in a single JSON
    object (pending a better solution, hopefully not implemented with the
    setup procedures model).
    
    We've also clearly reached the limits of the plaintext-based templating
    approach; this is about the best I could do without a JSON serializer
    (and a language capable of properly expressing rich data structures).
    
    Notice: Adding a "-DICOM" suffix to the name.  It's quite likely we'll
    have other plugins related to the GCP; for example a plugin to leverage
    GCP Object Storage as an Orthanc Storage backend.  We anticipate
    confusion around a plugin simply named "GCP" in that potential
    situation, and a possible rename of the plugin.  We hope to get ahead of
    this here.
    
    Resolves OPS-442

---
+++ docker/orthanc/setup.d/gcp-dicom.sh
@@ -0,0 +1,152 @@
+name=GCP_DICOM
+conf=gcp-dicom
+settings=(
+	ACCOUNTS
+	ACCOUNT_NAME
+	PROJECT
+	LOCATION
+	DATASET
+	DATASTORE
+	AUTHORIZED_SERVICE_ACCOUNT_FILE
+	AUTHORIZED_USER_FILE
+	AUTHORIZED_USER_CLIENT_ID
+	AUTHORIZED_USER_CLIENT_SECRET
+	AUTHORIZED_USER_REFRESH_TOKEN
+	BASE_URL
+)
+secrets=(
+	AUTHORIZED_USER_CLIENT_SECRET
+	AUTHORIZED_USER_REFRESH_TOKEN
+)
+plugin=libOrthancGoogleCloudPlatform
+function auth_methods_used {
+	local methods=(
+		AUTHORIZED_SERVICE_ACCOUNT_FILE
+		AUTHORIZED_USER_FILE
+		AUTHORIZED_USER_CLIENT_ID
+	)
+	local method methods_used=0
+	for method in "${methods[@]}"; do
+		if [[ ${!method} ]]; then
+			((methods_used++))
+		fi
+	done
+	echo $methods_used
+}
+function genconf {
+	if [[ $ACCOUNTS && ( \
+		$ACCOUNTS_NAME || \
+		$PROJECT || \
+		$LOCATION || \
+		$DATASET || \
+		$DATASTORE || \
+		$AUTHORIZED_SERVICE_ACCOUNT_FILE || \
+		$AUTHORIZED_USER_FILE || \
+		$AUTHORIZED_USER_CLIENT_ID || \
+		$AUTHORIZED_USER_CLIENT_SECRET || \
+		$AUTHORIZED_USER_REFRESH_TOKEN ) \
+	]]; then
+		err "Cannot set both ACCOUNTS and single-account mode settings"
+		return 1
+	fi
+	if (($(auth_methods_used) > 1)); then
+		err "Must use only one authentication method"
+		return 2
+	fi
+	BASE_URL=${BASE_URL:-https://healthcare.googleapis.com/v1beta1/}
+	if [[ $ACCOUNTS ]]; then
+		cat <<-EOF >"$1"
+		{
+			"GoogleCloudPlatform": {
+				"Accounts": ${ACCOUNTS:-"{}"},
+				"BaseUrl": "$BASE_URL"
+			}
+		}
+		EOF
+	elif [[ $ACCOUNT_NAME ]]; then
+		if [[ ! $PROJECT ]]; then
+			err "Missing PROJECT setting"
+			return 4
+		fi
+		if [[ ! $LOCATION ]]; then
+			err "Missing LOCATION setting"
+			return 5
+		fi
+		if [[ ! $DATASET ]]; then
+			err "Missing DATASET setting"
+			return 6
+		fi
+		if [[ ! $DATASTORE ]]; then
+			err "Missing DATASTORE setting"
+			return 7
+		fi
+		if [[ $AUTHORIZED_SERVICE_ACCOUNT_FILE ]]; then
+			cat <<-EOF >"$1"
+			{
+				"GoogleCloudPlatform": {
+					"Accounts": {
+						"$ACCOUNT_NAME": {
+							"Project": "$PROJECT",
+							"Location": "$LOCATION",
+							"Dataset": "$DATASET",
+							"DicomStore": "$DATASTORE",
+							"ServiceAccountFile": "$AUTHORIZED_SERVICE_ACCOUNT_FILE"
+						}
+					},
+					"BaseUrl": "$BASE_URL"
+				}
+			}
+			EOF
+		elif [[ $AUTHORIZED_USER_FILE ]]; then
+			cat <<-EOF >"$1"
+			{
+				"GoogleCloudPlatform": {
+					"Accounts": {
+						"$ACCOUNT_NAME": {
+							"Project": "$PROJECT",
+							"Location": "$LOCATION",
+							"Dataset": "$DATASET",
+							"DicomStore": "$DATASTORE",
+							"AuthorizedUserfile": "$AUTHORIZED_USER_FILE"
+						}
+					},
+					"BaseUrl": "$BASE_URL"
+				}
+			}
+			EOF
+		elif [[ $AUTHORIZED_USER_CLIENT_ID ]]; then
+			if [[ ! $AUTHORIZED_USER_CLIENT_SECRET ]]; then
+				err "Missing AUTHORIZED_USER_CLIENT_SECRET setting"
+				return 8
+			fi
+			if [[ ! $AUTHORIZED_USER_REFRESH_TOKEN ]]; then
+				err "Missing AUTHORIZED_USER_REFRESH_TOKEN setting"
+				return 9
+			fi
+			cat <<-EOF >"$1"
+			{
+				"GoogleCloudPlatform": {
+					"Accounts": {
+						"$ACCOUNT_NAME": {
+							"Project": "$PROJECT",
+							"Location": "$LOCATION",
+							"Dataset": "$DATASET",
+							"DicomStore": "$DATASTORE",
+							"AuthorizedUserClientId": "$AUTHORIZED_USER_CLIENT_ID",
+							"AuthorizedUserClientSecret": "$AUTHORIZED_USER_CLIENT_SECRET",
+							"AuthorizedUserRefreshToken": "$AUTHORIZED_USER_REFRESH_TOKEN"
+						}
+					},
+					"BaseUrl": "$BASE_URL"
+				}
+			}
+			EOF
+		else
+			err "Must set AUTHORIZED_USER_FILE or AUTHORIZED_USER_CLIENT_ID"
+			return 10
+		fi
+	else
+		err "Must set ACCOUNTS or ACCOUNT_NAME"
+		return 11
+	fi
+}
