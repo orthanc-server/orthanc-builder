aa376e41d3c2d2263f6251707235a1137d24756a
amazy <am@osimis.io>
2019-09-05 17:12:31 +0200

    added AC_AUTHENTICATION_ENABLED setting (default = false)

--- docker/orthanc/setup.d/access-control.sh
+++ docker/orthanc/setup.d/access-control.sh
@@ -1,11 +1,15 @@
 name=AC
 conf=remote-access
-settings=(ALLOW_REMOTE)
+settings=(
+	ALLOW_REMOTE 
+  AUTHENTICATION_ENABLED
+)
 default=true
 function genconf {
 	cat <<-EOF >"$1"
 	{
-		"RemoteAccessAllowed": ${ALLOW_REMOTE:-true}
+		"RemoteAccessAllowed": ${ALLOW_REMOTE:-true},
+		"AuthenticationEnabled": ${AUTHENTICATION_ENABLED:-false}
 	}
 	EOF
 }
