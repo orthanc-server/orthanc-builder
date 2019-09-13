b35575b299e59722e46219964f8eafaec59137c3
amazy <am@osimis.io>
2019-09-13 10:02:36 +0200

    reordered

--- docker/orthanc/setup.d/wvb.sh
+++ docker/orthanc/setup.d/wvb.sh
@@ -1,19 +1,19 @@
 name=WVB
 conf=osimis-webviewer
 settings=(
-	STUDY_DOWNLOAD_ENABLED
-	VIDEO_ENABLED
 	ANNOTATIONS_STORAGE_ENABLED
-	KEY_IMAGE_CAPTURE_ENABLED
-	KEYBOARD_SHORTCUTS_ENABLED
 	COMBINED_TOOL_ENABLED
 	DEFAULT_SELECTED_TOOL
-	LANGUAGE
-	TOGGLE_OVERLAY_TEXT_BUTTON_ENABLED
-	OPEN_ALL_PATIENT_STUDIES
-	SERIES_TO_IGNORE
 	DOWNLOAD_AS_JPEG_ENABLED
+	KEY_IMAGE_CAPTURE_ENABLED
+	KEYBOARD_SHORTCUTS_ENABLED
+	LANGUAGE
+	OPEN_ALL_PATIENT_STUDIES
 	PRINT_ENABLED
+	SERIES_TO_IGNORE
+	STUDY_DOWNLOAD_ENABLED
+	TOGGLE_OVERLAY_TEXT_BUTTON_ENABLED
+	VIDEO_ENABLED
 )
 plugin=libOsimisWebViewer
 
@@ -26,19 +26,19 @@ function genconf {
 	cat <<-EOF >"$1"
 	{
 		"WebViewer": {
-			"StudyDownloadEnabled": ${STUDY_DOWNLOAD_ENABLED:-true},
-			"DownloadAsJpegEnabled": ${DOWNLOAD_AS_JPEG_ENABLED:-false},
-			"PrintEnabled": ${PRINT_ENABLED:-true},
-			"VideoDisplayEnabled": ${VIDEO_ENABLED:-true},
 			"AnnotationStorageEnabled": ${ANNOTATIONS_STORAGE_ENABLED:-false},
+			"CombinedToolEnabled": ${COMBINED_TOOL_ENABLED:-false},
+			"DefaultLanguage": "${LANGUAGE:-en}",
+			"DefaultSelectedTool": "${DEFAULT_SELECTED_TOOL:-zoom}",
+			"DownloadAsJpegEnabled": ${DOWNLOAD_AS_JPEG_ENABLED:-false},
 			"KeyImageCaptureEnabled": ${KEY_IMAGE_CAPTURE_ENABLED:-false},
 			"KeyboardShortcutsEnabled": ${KEYBOARD_SHORTCUTS_ENABLED:-true},
-			"CombinedToolEnabled": ${COMBINED_TOOL_ENABLED:-false},
-			"DefaultSelectedTool": "${DEFAULT_SELECTED_TOOL:-zoom}",
-			"DefaultLanguage": "${LANGUAGE:-en}",
-			"ToggleOverlayTextButtonEnabled": ${TOGGLE_OVERLAY_TEXT_BUTTON_ENABLED:-false},
 			"OpenAllPatientStudies": ${OPEN_ALL_PATIENT_STUDIES:-true},
-			"SeriesToIgnore": ${SERIES_TO_IGNORE:-"{}"}
+			"PrintEnabled": ${PRINT_ENABLED:-true},
+			"SeriesToIgnore": ${SERIES_TO_IGNORE:-"{}"},
+			"StudyDownloadEnabled": ${STUDY_DOWNLOAD_ENABLED:-true},
+			"ToggleOverlayTextButtonEnabled": ${TOGGLE_OVERLAY_TEXT_BUTTON_ENABLED:-false},
+			"VideoDisplayEnabled": ${VIDEO_ENABLED:-true}
 		}
 	}
 	EOF
