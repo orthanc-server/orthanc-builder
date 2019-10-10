5d5f85868a2b505ca2d559323f52eb0e71221841
Alain Mazy <am@osimis.io>
2019-10-10 13:59:49 +0200

    removed one time password generation to match orthanc 1.5.8 new behaviour

--- docker/orthanc/setup.d/access-control.sh
+++ docker/orthanc/setup.d/access-control.sh
@@ -6,19 +6,12 @@ settings=(
 	REGISTERED_USERS
 )
 default=true
-if [ -z "$AC_REGISTERED_USERS" ]; then
-	defaultPassword=$(dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev)
-	defaultRegisteredUsers=""{\"orthanc\":\"$defaultPassword\"}""
-  if [ -z "$AC_AUTHENTICATION_ENABLED" ] || [  "$AC_AUTHENTICATION_ENABLED" == "true" ]; then
-	  echo "Since you've not defined any AC_REGISTERED_USERS, we've defined one for you: orthanc:$defaultPassword"
-  fi
-fi
 function genconf {
 	cat <<-EOF >"$1"
 	{
     "RemoteAccessAllowed": ${ALLOW_REMOTE:-true},
     "AuthenticationEnabled": ${AUTHENTICATION_ENABLED:-true},
-    "RegisteredUsers": ${REGISTERED_USERS:-"$defaultRegisteredUsers"}
+    "RegisteredUsers": ${REGISTERED_USERS:-"{}"}
 	}
 	EOF
 }
