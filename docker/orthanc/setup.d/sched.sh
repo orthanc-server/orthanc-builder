147c86b5718bd799493ddb69a57773e7bc75b3bc
Thibault Nélis <tn@osimis.io>
2020-02-28 11:03:13 +0100

    sched: Rename MAX_JOBS to MAX_QUEUED_JOBS
    Keep deprecated setting for backward compatibility.
    
    This is in anticipation of other settings (e.g. MAX_CONCURRENT_JOBS)
    which would introduce ambiguity without this clarification.

--- docker/orthanc/setup.d/sched.sh
+++ docker/orthanc/setup.d/sched.sh
@@ -1,10 +1,13 @@
 name=SCHED
 conf=scheduler
-settings=(MAX_JOBS SAVE_JOBS)
+settings=(MAX_JOBS MAX_QUEUED_JOBS SAVE_JOBS)
 function genconf {
+	# MAX_JOBS is deprecated
+	MAX_QUEUED_JOBS=${MAX_QUEUED_JOBS:-$MAX_JOBS}
+
 	cat <<-EOF >"$1"
 	{
-		"LimitJobs": ${MAX_JOBS:-10},
+		"LimitJobs": ${MAX_QUEUED_JOBS:-10},
 		"SaveJobs": ${SAVE_JOBS:-true}
 	}
 	EOF
