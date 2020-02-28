d6557c8181cc159216a10ab03adbb92db45aa149
Thibault Nélis <tn@osimis.io>
2020-02-28 11:04:17 +0100

    sched: Add MAX_CONCURRENT_JOBS setting

--- docker/orthanc/setup.d/sched.sh
+++ docker/orthanc/setup.d/sched.sh
@@ -1,6 +1,6 @@
 name=SCHED
 conf=scheduler
-settings=(MAX_JOBS MAX_QUEUED_JOBS SAVE_JOBS)
+settings=(MAX_JOBS MAX_QUEUED_JOBS MAX_CONCURRENT_JOBS SAVE_JOBS)
 function genconf {
 	# MAX_JOBS is deprecated
 	MAX_QUEUED_JOBS=${MAX_QUEUED_JOBS:-$MAX_JOBS}
@@ -8,6 +8,7 @@ function genconf {
 	cat <<-EOF >"$1"
 	{
 		"LimitJobs": ${MAX_QUEUED_JOBS:-10},
+		"ConcurrentJobs": ${MAX_CONCURRENT_JOBS:-2},
 		"SaveJobs": ${SAVE_JOBS:-true}
 	}
 	EOF
