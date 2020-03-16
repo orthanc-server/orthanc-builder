a2a4a1d60129214be2a5aa681b5558515628220d
Michel Rozpendowski <mro@osimis.io>
2020-03-16 21:53:32 +0100

    add SCHED_JOBS_HISTORY_SIZE

--- docker/orthanc/setup.d/sched.sh
+++ docker/orthanc/setup.d/sched.sh
@@ -1,6 +1,6 @@
 name=SCHED
 conf=scheduler
-settings=(MAX_JOBS MAX_QUEUED_JOBS MAX_CONCURRENT_JOBS SAVE_JOBS)
+settings=(MAX_JOBS MAX_QUEUED_JOBS MAX_CONCURRENT_JOBS SAVE_JOBS JOBS_HISTORY_SIZE)
 function genconf {
 	# MAX_JOBS is deprecated
 	MAX_QUEUED_JOBS=${MAX_QUEUED_JOBS:-$MAX_JOBS}
@@ -9,7 +9,8 @@ function genconf {
 	{
 		"LimitJobs": ${MAX_QUEUED_JOBS:-10},
 		"ConcurrentJobs": ${MAX_CONCURRENT_JOBS:-2},
-		"SaveJobs": ${SAVE_JOBS:-true}
+		"SaveJobs": ${SAVE_JOBS:-true},
+		"JobsHistorySize": ${JOBS_HISTORY_SIZE:-10}
 	}
 	EOF
 }
