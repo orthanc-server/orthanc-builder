19cc2073886cb75d16b8bd7935c0c72b501fe8d9
Michel Rozpendowski <mro@osimis.io>
2020-03-16 17:22:21 +0100

    transfers: add BUCKET_SIZE, CACHE_SIZE and MAX_PUSH_TRANSACTIONS

--- docker/orthanc/setup.d/transfers.sh
+++ docker/orthanc/setup.d/transfers.sh
@@ -1,6 +1,6 @@
 name=TRANSFERS
 conf=transfers
-settings=(THREADS RETRIES)
+settings=(THREADS RETRIES BUCKET_SIZE CACHE_SIZE MAX_PUSH_TRANSACTIONS)
 plugin=libOrthancTransfers
 function genconf {
     # TODO - Consider adding more options:
@@ -9,7 +9,10 @@ function genconf {
 	{
 		"Transfers": {
 			"Threads" : ${THREADS:-6},
-			"MaxHttpRetries" : ${RETRIES:-0}
+			"MaxHttpRetries" : ${RETRIES:-0},
+			"BucketSize": ${BUCKET_SIZE:-4096},
+			"CacheSize": ${CACHE_SIZE:-128},
+			"MaxPushTransactions": ${MAX_PUSH_TRANSACTIONS:-4}
 		}
 	}
 	EOF
