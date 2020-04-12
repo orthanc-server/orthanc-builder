30ce24bb83dd4069828caea2e5a4d21fdcdec2fd
Alain Mazy <am@osimis.io>
2020-04-12 10:15:59 +0200

    now building all open-source plugins

---
+++ new-build.ps1
@@ -0,0 +1,3 @@
+docker build -t osimis/orthanc-runner-base:current docker/orthanc-runner-base/
+docker build -t osimis/orthanc-builder-base:current docker/orthanc-builder-base/
+docker build -t osimis/orthanc-new:current docker/orthanc-builder-all/
