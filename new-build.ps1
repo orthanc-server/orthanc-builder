a52a8703024e9a26735aa853f79f20924502aefd
Alain Mazy <am@osimis.io>
2020-04-23 12:31:39 +0200

    fix

--- new-build.ps1
+++ new-build.ps1
@@ -1,5 +1,4 @@
 docker build -t osimis/orthanc-runner-base:current docker/orthanc-runner-base/
 docker build -t osimis/orthanc-builder-base:current docker/orthanc-builder-base/
-# docker build -t osimis/orthanc-new:current -f docker/orthanc-builder-all/Dockerfile docker/
 docker build -t osimis/orthanc:current -f docker/orthanc/Dockerfile docker/
-docker build -t osimis/orthanc-pro:curent -f docker/orthanc-pro/Dockerfile docker/
+docker build -t osimis/orthanc-pro:current -f docker/orthanc-pro/Dockerfile docker/
