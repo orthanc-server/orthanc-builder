4b26d241ab8df013bb8ae4cd456d645d8c35d75f
Sebastien Jodogne <s.jodogne@gmail.com>
2020-05-27 09:42:58 +0200

    added docker/linux-local-build.sh

---
+++ docker/linux-local-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script builds the "osimis/orthanc" and "osimis/orthanc-pro"
+# images on the local computer (as well as the base images for builds)
+
+set -ex
+
+docker build -t osimis/orthanc-runner-base:current ./orthanc-runner-base/
+docker build -t osimis/orthanc-builder-base:current ./orthanc-builder-base/
+docker build -t osimis/orthanc:current -f ./orthanc/Dockerfile ./
+docker build -t osimis/orthanc-pro:current -f ./orthanc-pro/Dockerfile ./
