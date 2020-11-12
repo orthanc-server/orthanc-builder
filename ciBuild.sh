483c46820429decf5fc77e3e00433a0a48ea0619
Alain Mazy <am@osimis.io>
2020-11-12 11:56:09 +0100

    playing with vcpkg cache

--- ciBuild.sh
+++ ciBuild.sh
@@ -70,7 +70,7 @@ if [[ $action == "build" ]]; then
 	docker tag osimis/orthanc-builder-base:current osimis/orthanc-builder-base:20.4.0
 
   docker build -t osimis/orthanc:current -f docker/orthanc/Dockerfile docker/orthanc/
-  docker build -t osimis/orthanc-pro:current --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -f docker/orthanc-pro-builder/Dockerfile docker/orthanc-pro-builder/
+  docker build -t osimis/orthanc-pro:current --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -f docker/orthanc-pro-builder/Dockerfile docker/orthanc-pro-builder/
 
   docker tag osimis/orthanc:current osimis/orthanc:$releaseTag
   docker tag osimis/orthanc-pro:current osimis/orthanc-pro:$releaseTag
