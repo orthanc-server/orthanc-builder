b9bdd2cfd077b5184b6056954a1060a531647227
Sebastien Jodogne <s.jodogne@gmail.com>
2020-12-03 08:38:59 +0100

    adding Stone Web viewer to OS X package

--- buildOrthancOSX.sh
+++ buildOrthancOSX.sh
@@ -40,6 +40,7 @@ wget "${URL}/PostgreSQL 3.2 - OS X Release/libOrthancPostgreSQLIndex.dylib"
 wget "${URL}/PostgreSQL 3.2 - OS X Release/libOrthancPostgreSQLStorage.dylib"
 wget "${URL}/Transfers 1.0 - OS X Release/libOrthancTransfers.dylib"
 wget "${URL}/WebViewer 2.6 - OS X Release/libOrthancWebViewer.dylib"
+wget "${URL}/Stone 1.0 - OS X Release/libStoneWebViewer.dylib"
 
 # Create the archive
 cd ${TARGET}
