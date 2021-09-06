c728c691b4a2be8d8f6f0d8da41df13c1c192e5b
Alain Mazy <alain@mazy.be>
2021-09-06 19:24:29 +0200

    fix win installer + tcia defaults

--- WindowsInstaller/ciBuildWindowsInstaller.sh
+++ WindowsInstaller/ciBuildWindowsInstaller.sh
@@ -4,8 +4,8 @@ set -x #to debug the script
 set -e #to exit the script at the first failure
 
 # build Windows 32 bits
-docker build -t installer-builder --build-arg configurationFile=Orthanc-32.json .
-dockerContainerId=$(docker create installer-builder)
+docker build -t installer-builder-32 --build-arg configurationFile=Orthanc-32.json .
+dockerContainerId=$(docker create installer-builder-32)
 
 # copy the orthanc.json generated from the 32 bits version (we can't generate it with wine and Orthanc 64 bits)
 docker cp $dockerContainerId:/tmp/OsimisInstaller/orthanc.json .
@@ -13,8 +13,8 @@ docker cp $dockerContainerId:/tmp/OsimisInstaller/OrthancInstaller-Win32-21.9.0.
 docker rm $dockerContainerId
 
 # build Windows 64 bits
-docker build -t installer-builder --build-arg configurationFile=Orthanc-64.json .
-dockerContainerId=$(docker create installer-builder)
+docker build -t installer-builder-64 --build-arg configurationFile=Orthanc-64.json .
+dockerContainerId=$(docker create installer-builder-64)
 docker cp $dockerContainerId:/tmp/OsimisInstaller/OrthancInstaller-Win64-21.9.0.exe .  # CHANGE_VERSION_WIN_INSTALLER
 docker rm $dockerContainerId
 
@@ -27,7 +27,10 @@ echo $AWS_ACCESS_KEY_ID
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 awsContainerId=$(docker create -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY anigeo/awscli s3 --region eu-west-1 cp /tmp/ s3://orthanc.osimis.io/win-installer/ --recursive --exclude "*" --include "OrthancInstaller*" --cache-control=max-age=1)
-docker cp . $awsContainerId:/tmp
+
+# CHANGE_VERSION_WIN_INSTALLER
+docker cp OrthancInstaller-Win32-21.9.0.exe $awsContainerId:/tmp
+docker cp OrthancInstaller-Win64-21.9.0.exe $awsContainerId:/tmp
 
 # upload
 docker start -a $awsContainerId
