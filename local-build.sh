4dd1a5ceb09d71049b365c876fbcf88b3e4a7e70
Alain Mazy <alain@mazy.be>
2021-11-08 17:26:32 +0100

    debian update cont

--- local-build.sh
+++ local-build.sh
@@ -1,4 +1,7 @@
-docker build -t osimis/orthanc-runner-base:current docker/orthanc-runner-base/
-docker build -t osimis/orthanc-builder-base:current docker/orthanc-builder-base/
-docker build -t osimis/orthanc:current -f docker/orthanc/Dockerfile docker/orthanc/
-# docker build --progress=plain -t osimis/orthanc-pro:current -f docker/orthanc-pro-builder/Dockerfile docker/orthanc-pro-builder/
+set -o errexit
+set -o xtrace
+
+docker build --progress=plain -t osimis/orthanc-runner-base:current docker/orthanc-runner-base/
+docker build --progress=plain -t osimis/orthanc-builder-base:current docker/orthanc-builder-base/
+docker build --progress=plain -t osimis/orthanc:current -f docker/orthanc/Dockerfile docker/orthanc/
+docker build --progress=plain -t osimis/orthanc-pro:current -f docker/orthanc-pro-builder/Dockerfile docker/orthanc-pro-builder/
