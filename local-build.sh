d172e2b0d58d4b9736582ad2b7ab6114143cf5fc
Alain Mazy <alain@mazy.be>
2022-03-25 17:36:34 +0100

    cleanup after moving out of bitbucket/jenkins

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,4 @@
 set -o errexit
 set -o xtrace
 
-# docker build --progress=plain -t osimis/orthanc-runner-base:current docker/orthanc-runner-base/
-# docker build --progress=plain -t osimis/orthanc-builder-base:current docker/orthanc-builder-base/
 docker build --progress=plain -t osimis/orthanc:current -f docker/orthanc/Dockerfile docker/orthanc/
-# docker build --progress=plain -t osimis/orthanc-pro:current -f docker/orthanc-pro-builder/Dockerfile docker/orthanc-pro-builder/
