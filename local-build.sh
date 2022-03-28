58717906f232cc39edcaeb513d17ea31ea94ce31
Alain Mazy <alain@mazy.be>
2022-03-28 22:17:23 +0200

    reorganizing docker build

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 set -o xtrace
 
-# docker build --progress=plain -t osimis/orthanc-runner-base:current docker/orthanc-runner-base/
-# docker build --progress=plain -t osimis/orthanc-builder-base:current docker/orthanc-builder-base/
-docker build --progress=plain -t osimis/orthanc:current -f docker/orthanc/Dockerfile docker/orthanc/
-# docker build --progress=plain -t osimis/orthanc-pro:current -f docker/orthanc-pro-builder/Dockerfile docker/orthanc-pro-builder/
+docker build --progress=plain -t osimis/orthanc-runner-base:current -f docker/orthanc/Dockerfile.runner-base docker/orthanc
+docker build --progress=plain -t osimis/orthanc-builder-base:current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-base docker/orthanc
+docker build --progress=plain -t osimis/orthanc-builder-vcpkg:current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-vcpkg docker/orthanc
+docker build --progress=plain -t osimis/orthanc:current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile docker/orthanc/
