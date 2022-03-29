ababede37dfe5c9657e8c9089dadf5eb33dd90fc
Alain Mazy <alain@mazy.be>
2022-03-29 09:12:29 +0200

    split build in more steps to avoit timeouts and benefit from cache

--- local-build.sh
+++ local-build.sh
@@ -2,6 +2,11 @@ set -o errexit
 set -o xtrace
 
 docker build --progress=plain -t osimis/orthanc-runner-base:current -f docker/orthanc/Dockerfile.runner-base docker/orthanc
+
 docker build --progress=plain -t osimis/orthanc-builder-base:current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-base docker/orthanc
-docker build --progress=plain -t osimis/orthanc-builder-vcpkg:current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-vcpkg docker/orthanc
+
+docker build --progress=plain -t osimis/orthanc-builder-base:vcpkg-current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-vcpkg --target orthanc-build-vcpkg docker/orthanc
+docker build --progress=plain -t osimis/orthanc-builder-base:vcpkg-google-current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-vcpkg --target orthanc-build-vcpkg-google docker/orthanc
+docker build --progress=plain -t osimis/orthanc-builder-base:vcpkg-azure-current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile.builder-vcpkg --target orthanc-build-vcpkg-azure docker/orthanc
+
 docker build --progress=plain -t osimis/orthanc:current --build-arg BASE_IMAGE_TAG=current -f docker/orthanc/Dockerfile docker/orthanc/
