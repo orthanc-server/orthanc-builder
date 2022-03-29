5806026ad6a842f1d063f84fd341a96ac7daa0f8
Alain Mazy <alain@mazy.be>
2022-03-29 15:53:08 +0200

    add integration tests

---
+++ docker/integration-tests/run-integration-tests.sh
@@ -0,0 +1,12 @@
+set -o errexit
+set -o xtrace
+
+branch_tag_name=${1:-unknown}
+
+docker build --build-arg IMAGE_TAG=$branch_tag_name -f orthanc-under-tests/Dockerfile -t orthanc-under-tests orthanc-under-tests
+
+# CHANGE_ORTHANC_TESTS_VERSION
+docker build --build-arg ORTHANC_TESTS_REVISION=default -f orthanc-tests/Dockerfile -t orthanc-tests orthanc-tests
+
+
+COMPOSE_FILE=docker-compose.sqlite.yml                   docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
