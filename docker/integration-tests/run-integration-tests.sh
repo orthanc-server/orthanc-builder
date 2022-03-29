f130ff77a0e6f9ea1ba1634671a4c344ff8c464d
Alain Mazy <alain@mazy.be>
2022-03-29 16:55:58 +0200

    more integ tests

--- docker/integration-tests/run-integration-tests.sh
+++ docker/integration-tests/run-integration-tests.sh
@@ -9,7 +9,26 @@ docker build --build-arg IMAGE_TAG=$branch_tag_name -f orthanc-under-tests/Docke
 docker build --build-arg ORTHANC_TESTS_REVISION=default -f orthanc-tests/Dockerfile -t orthanc-tests orthanc-tests
 
 
-COMPOSE_FILE=docker-compose.sqlite.yml                   docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
+COMPOSE_FILE=docker-compose.sqlite.yml                      docker-compose down -v
+COMPOSE_FILE=docker-compose.sqlite.yml                      docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
 
+COMPOSE_FILE=docker-compose.postgres.yml                    docker-compose down -v
+COMPOSE_FILE=docker-compose.postgres.yml                    docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
 
-# TODO run other tes
+COMPOSE_FILE=docker-compose.odbc-postgres.yml               docker-compose down -v
+COMPOSE_FILE=docker-compose.odbc-postgres.yml               docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
+
+COMPOSE_FILE=docker-compose.odbc-sqlite.yml                 docker-compose down -v
+COMPOSE_FILE=docker-compose.odbc-sqlite.yml                 docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
+
+COMPOSE_FILE=docker-compose.mysql.yml                       docker-compose down -v
+COMPOSE_FILE=docker-compose.mysql.yml                       docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
+
+# TODO run other tests
+# TODO: reenable sqlserver odbc drivers
+# COMPOSE_FILE=docker-compose.odbc-sql-server.yml          docker-compose down -v
+# COMPOSE_FILE=docker-compose.odbc-sql-server.yml          docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
+
+# note: not functional yet:
+# COMPOSE_FILE=docker-compose.odbc-mysql.yml               docker-compose down -v
+# COMPOSE_FILE=docker-compose.odbc-mysql.yml               docker-compose up --build --exit-code-from orthanc-tests --abort-on-container-exit
