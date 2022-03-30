52a6dbc2ade1b932dc783adce77a3140b3faa80e
Alain Mazy <alain@mazy.be>
2022-03-30 14:55:11 +0200

    Osx (#2)
    build OSX binaries + package

--- README.md
+++ README.md
@@ -11,10 +11,17 @@ Windows Installer and OSX package are collecting build artifacts from the Orthan
 
 # Where to find the releases ?
 
-- [Docker image](https://hub.docker.com/r/osimis/orthanc)
+- [Docker image (linux/amd64)](https://hub.docker.com/r/osimis/orthanc)
 - [Windows 64 bits installer](https://orthanc.osimis.io/win-installer/OrthancInstaller-Win64-latest.exe)
 - [Windows 32 bits installer](https://orthanc.osimis.io/win-installer/OrthancInstaller-Win32-latest.exe)
-- [OSX package (Intel)](https://orthanc.osimis.io/osx/stable/orthancAndPluginsOSX.stable.zip)
+- [OSX package (Universal)](https://orthanc.osimis.io/osx/stable/orthancAndPluginsOSX.stable.zip)
+
+**Notes**: 
+- you can use this repo to build `linux/arm64` docker images but we are currently not able to build them on our build slaves because, with QEMU emulation, a build would take more than 12
+  hours which is the limit of github.
+- the OSX binaries are all Universal binaries except for the OsimisViewer
+  and WSI plugins that are Intel binaries only.
+
 
 # Contributions
 
--- buildOrthancOSX.sh
+++ buildOrthancOSX.sh
@@ -3,20 +3,30 @@
 set -ex
 
 is_tag=${1:-false}
+<<<<<<< HEAD
+branch_tag_name=${2:-22.3.0}       # CHANGE_VERSION_OSX
+download_from_buildbot=${3:-true}  # if false, we consider that binaries are already in /tmp/osx-package/Orthanc-OSX-$branch_tag_name
+
+=======
 branch_tag_name=${2:-unknown}
 
 # CHANGE_VERSION_OSX
 VERSION=22.3.0
+>>>>>>> master
 
 URL='https://alain:koo4oCah@buildbot.orthanc-server.com/artifacts/Binaries/'
-TARGET='/tmp/'
-FOLDER=Orthanc-OSX-$VERSION
+TARGET='/tmp/osx-package/'
+FOLDER=Orthanc-OSX-$branch_tag_name
+
 
 # https://stackoverflow.com/a/4774063/881731
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-rm -rf ${TARGET}/${FOLDER} 
-mkdir ${TARGET}/${FOLDER}
+if [[ $download_from_buildbot == "true" ]]; then
+    rm -rf ${TARGET}/${FOLDER} 
+    mkdir -p ${TARGET}/${FOLDER}
+fi
+
 cd ${TARGET}/${FOLDER}
 
 # Resources
@@ -25,40 +35,49 @@ cp ${SCRIPTPATH}/orthancBuildResources/readmeOSX.txt readme.txt
 cp ${SCRIPTPATH}/orthancBuildResources/startOrthanc.command .
 cp ${SCRIPTPATH}/WindowsInstaller/Resources/ca-certificates.crt .
 
-# Orthanc core
-wget "${URL}/Orthanc 1.10.1 - OS X Release/Orthanc"
-wget "${URL}/Orthanc 1.10.1 - OS X Release/libModalityWorklists.dylib"
-wget "${URL}/Orthanc 1.10.1 - OS X Release/libServeFolders.dylib"
-wget "${URL}/Orthanc 1.10.1 - OS X Release/libConnectivityChecks.dylib"
+if [[ $download_from_buildbot == "true" ]]; then
+
+    # Orthanc core
+    wget "${URL}/Orthanc 1.10.1 - OS X Release/Orthanc"
+    wget "${URL}/Orthanc 1.10.1 - OS X Release/libModalityWorklists.dylib"
+    wget "${URL}/Orthanc 1.10.1 - OS X Release/libServeFolders.dylib"
+    wget "${URL}/Orthanc 1.10.1 - OS X Release/libConnectivityChecks.dylib"
+
+    # Plugins
+    wget "${URL}/DicomWeb 1.7 - OS X Release/libOrthancDicomWeb.dylib"
+    wget "${URL}/MySQL 4.3 - OS X Release/libOrthancMySQLIndex.dylib"
+    wget "${URL}/MySQL 4.3 - OS X Release/libOrthancMySQLStorage.dylib"
+    wget "${URL}/PostgreSQL 4.0 - OS X Release/libOrthancPostgreSQLIndex.dylib"
+    wget "${URL}/PostgreSQL 4.0 - OS X Release/libOrthancPostgreSQLStorage.dylib"
+    wget "${URL}/Transfers 1.0 - OS X Release/libOrthancTransfers.dylib"
+    wget "${URL}/WebViewer 2.8 - OS X Release/libOrthancWebViewer.dylib"
+    wget "${URL}/Stone 2.3 - OS X Release/libStoneWebViewer.dylib"
+    wget "${URL}/Gdcm 1.5 - OS X Release/libOrthancGdcm.dylib"
+    wget "${URL}/Odbc 1.1 - OS X Release/libOrthancOdbcIndex.dylib"
+    wget "${URL}/Odbc 1.1 - OS X Release/libOrthancOdbcStorage.dylib"
+    wget "${URL}/Tcia 1.1 - OS X Release/libOrthancTcia.dylib"
+    wget "${URL}/FolderIndexer 1.0 - OS X Release/libOrthancIndexer.dylib"
+
+    # these plugins are not built by GitHub so download but only contain AMD64 binary (TODO)
+    wget "${URL}/OsimisWebViewer 1.4.2 - OS X Release/libOsimisWebViewer.dylib"
+    wget "${URL}/WSI 1.1 - OS X Release/libOrthancWSI.dylib"
+fi
+
+
 
 chmod +x ./Orthanc
-
-# Plugins
-wget "${URL}/DicomWeb 1.7 - OS X Release/libOrthancDicomWeb.dylib"
-wget "${URL}/MySQL 4.3 - OS X Release/libOrthancMySQLIndex.dylib"
-wget "${URL}/MySQL 4.3 - OS X Release/libOrthancMySQLStorage.dylib"
-wget "${URL}/OsimisWebViewer 1.4.2 - OS X Release/libOsimisWebViewer.dylib"
-wget "${URL}/PostgreSQL 4.0 - OS X Release/libOrthancPostgreSQLIndex.dylib"
-wget "${URL}/PostgreSQL 4.0 - OS X Release/libOrthancPostgreSQLStorage.dylib"
-wget "${URL}/Transfers 1.0 - OS X Release/libOrthancTransfers.dylib"
-wget "${URL}/WebViewer 2.8 - OS X Release/libOrthancWebViewer.dylib"
-wget "${URL}/Stone 2.3 - OS X Release/libStoneWebViewer.dylib"
-wget "${URL}/Gdcm 1.5 - OS X Release/libOrthancGdcm.dylib"
-wget "${URL}/WSI 1.1 - OS X Release/libOrthancWSI.dylib"
-wget "${URL}/Odbc 1.1 - OS X Release/libOrthancOdbcIndex.dylib"
-wget "${URL}/Odbc 1.1 - OS X Release/libOrthancOdbcStorage.dylib"
-wget "${URL}/Tcia 1.1 - OS X Release/libOrthancTcia.dylib"
-wget "${URL}/FolderIndexer 1.0 - OS X Release/libOrthancIndexer.dylib"
 
 # Create the archive
 cd ${TARGET}
 zip -r ${FOLDER}.zip ${FOLDER}
 echo -e "\nThe archive can be found at: ${TARGET}/${FOLDER}.zip\n"
 
-
 # upload files to AWS
 #####################
 
+<<<<<<< HEAD
+aws s3 --region eu-west-1 cp /tmp/osx-package/ s3://orthanc.osimis.io/osx/releases/ --recursive --exclude "*" --include "Orthanc-OSX*.zip" --cache-control=max-age=1
+=======
 # we first need to create the container before we can copy files to it
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
@@ -66,10 +85,15 @@ awsContainerId=$(docker create -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY ani
 
 # CHANGE_VERSION_WIN_INSTALLER
 docker cp ${TARGET}/${FOLDER}.zip $awsContainerId:/tmp
+>>>>>>> master
 
-# upload
-docker start -a $awsContainerId
+if [[ $is_tag == "true" ]]; then
+    
+    cp ${TARGET}/${FOLDER}.zip ${TARGET}/orthancAndPluginsOSX.stable.zip
+    aws s3 --region eu-west-1 cp /tmp/osx-package/ s3://orthanc.osimis.io/osx/stable/ --recursive --exclude "*" --include "orthancAndPluginsOSX*" --cache-control=max-age=1
 
+<<<<<<< HEAD
+=======
 # remove container
 docker rm $awsContainerId
 
@@ -83,4 +107,5 @@ if [[ $is_tag == "true" ]]; then
 
     # remove container
     docker rm $awsContainerId
+>>>>>>> master
 fi
