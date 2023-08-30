774108e7c42f41bddc80cd72ab5088eb12e06609
Alain Mazy <alain@mazy.be>
2023-08-30 12:35:40 +0200

    fix python version

--- docker/orthanc/build-or-download.sh
+++ docker/orthanc/build-or-download.sh
@@ -132,7 +132,7 @@ elif [[ $target == "orthanc-python" ]]; then
 
         hg clone https://hg.orthanc-server.com/orthanc-python/ -r $commitId $sourcesRootPath
         pushd $buildRootPath
-        cmake -DALLOW_DOWNLOADS=ON -DCMAKE_BUILD_TYPE:STRING=Release -DUSE_SYSTEM_GOOGLE_TEST=ON -DUSE_SYSTEM_ORTHANC_SDK=OFF -DPYTHON_VERSION=3.9 $sourcesRootPath
+        cmake -DALLOW_DOWNLOADS=ON -DCMAKE_BUILD_TYPE:STRING=Release -DUSE_SYSTEM_GOOGLE_TEST=ON -DUSE_SYSTEM_ORTHANC_SDK=OFF -DPYTHON_VERSION=3.11 $sourcesRootPath
         make -j 4
 
         upload libOrthancPython.so
