ffb1f432c089ddacd6134bb9910895210e476183
Alain Mazy <am@orthanc.team>
2026-03-10 17:36:56 +0100

    try fix for good !

--- local-build.sh
+++ local-build.sh
@@ -255,7 +255,7 @@ else
     buildTargets="build-orthanc-tests $finalImageTarget"
 
     # save the orthanc-tests archive path for runners who are not able to run hg clone
-    echo "https://public-files.orthanc.team/tmp-builds/docker-builds/$platform/$BASE_UBUNTU_IMAGE-$version/$ORTHANC_TESTS_COMMIT_ID-orthanc-tests-tar.gz" > "/tmp/$version-orthanc-tests-archive-path"
+    echo "https://public-files.orthanc.team/tmp-builds/docker-builds/$platform/$BASE_UBUNTU_IMAGE-$version/$ORTHANC_TESTS_COMMIT_ID-orthanc-tests.tar.gz" > "/tmp/$version-orthanc-tests-archive-path"
     cat "/tmp/$version-orthanc-tests-archive-path"
     prefer_downloads=1
     enable_upload=1
