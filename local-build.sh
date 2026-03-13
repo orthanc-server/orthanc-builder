de72c6bcd6f1761fb9f8e854ca79c10476a0bc59
Alain Mazy <am@orthanc.team>
2026-03-13 11:50:01 +0100

    fix

--- local-build.sh
+++ local-build.sh
@@ -184,6 +184,7 @@ if [[ $step == "generate-commit-id-matrix" ]] || [[ $getCommitIdsFromFile == "fa
     fi
 
     if [[ $version == "unstable" ]]; then
+        wget https://public-files.orthanc.team/tmp-builds/hg-repos/orthanc-$ORTHANC_COMMIT_ID.tar.gz --output-document /tmp/orthanc-$ORTHANC_COMMIT_ID.tar.gz --quiet
         aws s3 --region eu-west-1 cp /tmp/orthanc-$ORTHANC_COMMIT_ID.tar.gz s3://public-files.orthanc.team/third-party-downloads/Orthanc-mainline.tar.gz --cache-control=max-age=1 --quiet
     fi
 
